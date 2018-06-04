870c9569d68b380532cc040feb02a5d413e8a778
Marcelo Magano <marcelo.magano99@gmail.com>
2018-06-04 19:54:12 +0100

    Added Unit test

---
+++ StarWarsGraphQLNetCore.Tests.Unit/Data/EntityFramework/Repositories/DroidRepositoryShould.cs
@@ -0,0 +1,41 @@
+﻿using StarWarsGraphQLNetCore.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using StarWarsGraphQLNetCore.Data.EntityFramework;
+using StarWarsGraphQLNetCore.Data.EntityFramework.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace StarWarsGraphQLNetCore.Tests.Unit.Data.EntityFramework.Repositories
+{
+    public class DroidRepositoryShould
+    {
+        private readonly DroidRepository _droidRepository;
+        public DroidRepositoryShould()
+        {
+            // Given
+            // https://docs.microsoft.com/en-us/ef/core/miscellaneous/testing/in-memory
+            var options = new DbContextOptionsBuilder<StarWarsContext>()
+                .UseInMemoryDatabase(databaseName: "StarWars")
+                .Options;
+            using (StarWarsContext context = new StarWarsContext(options))
+            {
+                context.Droids.Add(new Droid { Id = 1, Name = "R2-D2" });
+                context.SaveChanges();
+            }
+            StarWarsContext starWarsContext = new StarWarsContext(options);
+            _droidRepository = new DroidRepository(starWarsContext);
+        }
+
+        [Fact]
+        public async void ReturnR2D2DroidGivenIdOf1()
+        {
+            //When
+            Droid droid = await _droidRepository.Get(1);
+            //Then
+            Assert.NotNull(droid);
+            Assert.Equal("R2-D2", droid.Name);
+        }
+    }
+}
---
+++ StarWarsGraphQLNetCore.Tests.Unit/Data/InMemory/DroidRepositoryShould.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using StarWarsGraphQLNetCore.Data.InMemory;
+using StarWarsGraphQLNetCore.Core.Models;
+
+namespace StarWarsGraphQLNetCore.Tests.Unit.Data.InMemory
+{
+    public class DroidRepositoryShould
+    {
+        private readonly DroidRepository _droidRepository;
+        public DroidRepositoryShould()
+        {
+            _droidRepository = new DroidRepository();
+        }
+
+        [Fact]
+        public async void ReturnR2D2DroidGivenIdOf1()
+        {
+            Droid droid = await _droidRepository.Get(1);
+
+            Assert.NotNull(droid);
+            Assert.Equal("R2-D2", droid.Name);
+        }
+    }
+}
