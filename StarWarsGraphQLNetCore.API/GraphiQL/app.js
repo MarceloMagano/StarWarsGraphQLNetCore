ed811f7f834fcfbbd954d5c5197a360f82c59681
Marcelo Magano <marcelo.magano99@gmail.com>
2018-06-04 16:02:17 +0100

    Graphiql now showing up on the browser. moved package.json and webpack.config to the root folder because for some reason some node_modules were bundled and i was getting erros on the bundle process

--- StarWarsGraphQLNetCore.API/GraphiQL/app.js
+++ StarWarsGraphQLNetCore.API/GraphiQL/app.js
@@ -1,19 +1,22 @@
 ﻿import React from 'react'
 import ReactDOM from 'react-dom'
 import GraphiQL from 'graphiql'
-import fetch from 'isomorphic-fetch'
+import axios from 'axios'
 import 'graphiql/graphiql.css'
 import './app.css'
 
 function graphQLFetcher(graphQLParams) {
-  return fetch(window.location.origin + '/graphql', {
-    method: 'post',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(graphQLParams)
-  }).then(response => response.json())
+  return axios({
+    method: 'POST',
+    url: window.location.origin + '/graphql',
+    data: graphQLParams
+  }).then(resp => resp.data)
+
+  // return fetch(window.location.origin + '/graphql', {
+  //   method: 'post',
+  //   headers: { 'Content-Type': 'application/json' },
+  //   body: JSON.stringify(graphQLParams)
+  // }).then(response => response.json())
 }
 
-ReactDOM.render(
-  <GraphiQL fetcher={graphQLFetcher} />,
-  document.getElementById('app')
-)
+ReactDOM.render(<GraphiQL fetcher={graphQLFetcher}/>, document.getElementById('app'))
